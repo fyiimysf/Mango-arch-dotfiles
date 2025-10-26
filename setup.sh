a2e7f213d1f808a871e00c2fa0628dba184334bf
Mohammad Yousaf <m.y5f.mohammad@gmail.com>
2025-10-26 10:57:02 +0500

    created stow directory

--- setup.sh
+++ setup.sh
@@ -225,15 +225,18 @@ echo -e "${GREEN}All packages installed successfully!${NC}"
 # Step 6: Stow all subdirectories
 echo ""
 echo "Stowing all dotfiles from $DOTFILES_DIR..."
+
+	cd stow
 for dir in */; do
     package="${dir%/}"  # Remove trailing slash
     if [[ -d "$package" ]]; then
-        if [ ! "$package" = "setup.sh" ]&[ ! "$package" = "README.md" ]&[ ! "$package" = ".gitattributes" ]&[ ! "$package" = ".gitignore" ]&[ ! "$package" = "LICENSE" ]&[ ! "$package" = ".git" ]; then
+        #if [ ! "$package" = "setup.sh" ]&[ ! "$package" = "README.md" ]&[ ! "$package" = ".gitattributes" ]&[ ! "$package" = ".gitignore" ]&[ ! "$package" = "LICENSE" ]&[ ! "$package" = ".git" ]; then
          echo "Stowing $package..."
-         stow -v "$package"
-	       fi
+         stow -v -t '../../' "$package"
+	#fi
     fi
 done
+	cd ..
 
 echo -e "${GREEN}Stowing complete!${NC}"
 
